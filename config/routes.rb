cb3cac331dfc7cd01b606ffa9f5ae96719faecd3
sumit-jr <er.sahsumit@gmail.com>
2025-01-11 21:39:49 +0545

    feat: Added AdminController with authentication and customized routes for Admin users. Removed  module from Admin Devise model. Updated routes for Admin access.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  devise_for :admins, skip: [ :registrations ]
   devise_for :users
   resources :movies
   # Render dynamic PWA files from app/views/pwa/*
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+
+  authenticated :admin_user do
+    root to: "admin#index", as: :admin_root
+  end
+
+  get "admin" => "admin#index"
 
   # Defines the root path route ("/")
   root "movies#index"
