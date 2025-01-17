e0c76b95dabe9c8bbe29e8f302cba40081cd5b3c
sumit-jr <er.sahsumit@gmail.com>
2025-01-17 06:41:21 +0545

    feat: Add paid content functionality and enhance movie details

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_15_002856) do
+ActiveRecord::Schema[7.2].define(version: 2025_01_16_020300) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -84,6 +84,9 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_15_002856) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "paid"
+    t.string "stripe_price_id"
+    t.text "premium_description"
   end
 
   create_table "users", force: :cascade do |t|
