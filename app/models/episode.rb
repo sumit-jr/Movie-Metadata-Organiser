c801ac541c8d56029b66ecf8452fecc946759dc5
sumit-jr <er.sahsumit@gmail.com>
2025-01-29 21:15:46 +0545

    feat: Implement CRUD functionality for Admin Episodes
    - Added create, edit, update, and destroy actions for admin episodes.
    - Enhanced episode form with rich text description and video upload.
    - Improved navigation links for better user experience.
    - Ensured strong parameters for episode attributes.

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -3,6 +3,7 @@ class Episode < ApplicationRecord
     attachable.variant :thumb, resize_to_limit: [ 500, 500 ]
   end
   acts_as_list
+  has_rich_text :description
   belongs_to :movie
   has_many :episode_users, dependent: :destroy
 
